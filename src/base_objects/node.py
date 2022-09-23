7e35db87336103632a32ccfa23973f9a188733d1
Yury Stolpov <yurass130@gmail.com>
2022-09-23 11:45:21 +0300

    Revert "Add: NodeCreator prototype of metaclass for cretion of nodes."
    This reverts commit e7cd7008b31bc420aa81b7448318ab28e14c17aa.

--- src/base_objects/node.py
+++ src/base_objects/node.py
@@ -1,9 +1,3 @@
-class NodeCreator(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(NodeCreator, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
 
 class Node:
     def __init__(self, name= None, **kwargs):
